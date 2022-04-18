fc8f2d7a52e08b7afd0d80270f343f112b84c463
badcel <1218031+badcel@users.noreply.github.com>
2022-04-18 09:21:32 +0200

    Add Gtk4 drawing area sample

---
+++ src/Samples/Gtk4/DrawingArea/DrawingArea.cs
@@ -0,0 +1,14 @@
+﻿using Gtk;
+
+var application = Application.New("org.gir.core", Gio.ApplicationFlags.FlagsNone);
+application.OnActivate += (sender, args) =>
+{
+    var drawingArea = DrawingArea.New();
+    drawingArea.SetDrawFunc(); //TODO
+    
+    var window = ApplicationWindow.New((Application) sender);
+    window.Title = "DrawingArea Demo";
+    window.Child = drawingArea;
+    window.Show();
+};
+return application.Run();
